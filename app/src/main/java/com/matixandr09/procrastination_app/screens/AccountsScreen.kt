a30ca6fe359f14d7f881e0ad3f8aec6d055879f6
MatixAndr09 <matixandr09@gmail.com>
2025-10-04 20:26:55 +0200

    Fixed build issues

--- app/src/main/java/com/matixandr09/procrastination_app/screens/AccountsScreen.kt
+++ app/src/main/java/com/matixandr09/procrastination_app/screens/AccountsScreen.kt
@@ -1,22 +1,33 @@
 package com.matixandr09.procrastination_app.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AccountsScreen(navController: NavController) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        IconButton(onClick = { navController.popBackStack() }) {
-            Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Accounts Screen") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                    }
+                }
+            )
         }
-        Text("Accounts Screen")
+    ) { innerPadding ->
+        Text("Accounts Screen Content", modifier = Modifier.padding(innerPadding))
     }
 }
