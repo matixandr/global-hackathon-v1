4436a34a2e35e3a5b7e4f2318ff370ab6267e64a
MatixAndr09 <matixandr09@gmail.com>
2025-10-04 20:51:48 +0200

    Added navbar to MainScreen.kt

--- app/src/main/java/com/matixandr09/procrastination_app/screens/MainScreen.kt
+++ app/src/main/java/com/matixandr09/procrastination_app/screens/MainScreen.kt
@@ -1,27 +1,59 @@
 package com.matixandr09.procrastination_app.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.matixandr09.procrastination_app.R
 
 @Composable
 fun MainScreen(navController: NavController) {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding(),
     ) {
-        Button(onClick = { navController.navigate("accounts") }) {
-            Text("Accounts")
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.account),
+                contentDescription = "Account"
+            )
+            Text(text = "Dynamic Text") // This will be dynamic in the future
+            Image(
+                painter = painterResource(id = R.drawable.streak),
+                contentDescription = "Streak"
+            )
         }
-        Button(onClick = { navController.navigate("streak") }) {
-            Text("Streak")
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = { navController.navigate("accounts") }) {
+                Text("Accounts")
+            }
+            Button(onClick = { navController.navigate("streak") }) {
+                Text("Streak")
+            }
         }
     }
 }
